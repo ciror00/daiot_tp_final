507910fdec5a068722f614255a1546a07f985358
ciror00 <ciror00@gmail.com>
2020-10-07 15:05:59 -0300

    Se crea modelo

---
+++ Orchestrator/app/Device.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Se crea el Modelo para administrar la base de datos.
+// Desde este modelo se mapea la table y se limpia el codigo para hacer consultas transparentes
+class Device extends Model
+{
+    // Se crea la propiedad "fillable" para agregar las columnas que queremos insertar de la DB
+    protected $fillable = ['uuid', 'enable'];
+}
