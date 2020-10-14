96d0de27d1a49c1bfaf434fd4d259249578b4676
ciror00 <ciror00@gmail.com>
2020-10-14 20:17:30 -0300

    Boton para navegar entre las pantallas

--- Orchestrator/resources/views/partials/nav-header-main.blade.php
+++ Orchestrator/resources/views/partials/nav-header-main.blade.php
@@ -1,23 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="{{ route('home') }}">Home</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
+            <!--
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    Opciones
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" 
+                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Ir a
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('device.index') }}">Dispositivos</a>
+                    <a class="dropdown-item" href="{{ route('home') }}">Mediciones</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Mediciones</a>
+                    <a class="dropdown-item" href="{{ route('device.index') }}">Dispositivos</a>
                 </div>
             </li>
+            -->
+            <a class="dropdown-item" href="{{ route('home') }}">Mediciones</a>
         </ul>
 
         <ul class="navbar-nav">
--- Orchestrator/resources/views/web/partials/nav-header.blade.php
+++ Orchestrator/resources/views/web/partials/nav-header.blade.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-light bg-dark mb-4">
-  <a class="navbar-brand text-white" href="#">CMSS
+  <a class="navbar-brand text-white" href="./">CMSS
   <small class="navbar-brand text-white">(Comprehensive Measurement Sensor System)</small>
   </a>
-  <a class="navbar-brand text-white" href="#">
-    <img src="https://cys.com.ar/wp-content/uploads/2015/10/logo_retina.png" width="30" height="30" class="d-inline-block align-top" alt="">
-  </a>
+  <a class="navbar-brand text-white" href="/admin/device">Admin</a>
 </nav>
